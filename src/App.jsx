f598b316ab90304287840a77b11f7f3ac8552a7a
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-04-16 14:45:07 +0200

    ✨ F02: APP > Create Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import LoginPage from './components/pages/loginPage/LoginPage'
+import Orderpage from './components/pages/orderPage/Orderpage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ErrorPage from './components/pages/errorPage/ErrorPage'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <LoginPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage/>} />
+        <Route path='/orderpage' element={<Orderpage/>} />
+        <Route path='/errorpage' element={<ErrorPage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
