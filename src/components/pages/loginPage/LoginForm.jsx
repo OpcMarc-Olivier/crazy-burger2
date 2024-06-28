0c1a7ca61ad86b9c3e4f21e383d22d4f5c2cc828
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-06-28 09:07:41 +0200

    ✨Add image project

--- src/components/pages/loginPage/LoginForm.jsx
+++ src/components/pages/loginPage/LoginForm.jsx
@@ -1,36 +1,136 @@
-import React, { useState } from 'react'
-import {Link, NavLink, useNavigate} from 'react-router-dom';
-
-
-
+import React, { useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../theme/index";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { FaChevronRight } from "react-icons/fa6";
 
 function LoginForm() {
     //state
-const [inputValue,setInputValue]= useState ("");
-const navigate = useNavigate ()
-//comportement
+    const [inputValue, setInputValue] = useState("");
+    const navigate = useNavigate();
+    //comportement
 
-const handleSubmit = (e)=>{
-  e.preventDefault()
-  
-  setInputValue("")
-  navigate(`order/${inputValue}`)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setInputValue("");
+        navigate(`order/${inputValue}`);
+    };
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setInputValue(e.target.value);
+    };
+    return (
+        <LoginFormStyled action="" onSubmit={handleSubmit}>
+            <h2>Bienvenue chez nous!</h2>
+            <br />
+            <div className="cta-container">
+                <h3>Connectez-vous</h3>
+                <div className="cta-input-button">
+                    <div className="input-wrapper">
+                        <IoPersonCircleOutline className="icon-person-circle" />
+                        <input
+                            required
+                            onChange={handleChange}
+                            value={inputValue}
+                            type=""
+                            placeholder="Entrer votre prénom"
+                        />
+                    </div>
+                    <button>
+                        <span>Accéder à mon espace</span>
+                        <FaChevronRight className="arrow-right" />
+                    </button>
+                </div>
+            </div>
+        </LoginFormStyled>
+    );
 }
 
-const handleChange = (e)=>{
-  console.log(e.target.value)
-  setInputValue(e.target.value)
-  
-}
-  return (
-    <form action="" onSubmit={handleSubmit}>
-        <h1>Bienvenue chez nous!</h1>
-        <br />
-        <h2>Connectez-vous</h2>
-        <input required onChange={handleChange} value={inputValue} type="" placeholder="Entrer votre prénom" />
-        <button>Accéder à votre espace</button>
-    </form>
-  )
-}
+const LoginFormStyled = styled.form`
+    width: 400px;
+    color: ${theme.colors.white};
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    /* border: 1px solid red; */
+    /* background: #566844; */
 
-export default LoginForm
+    h2 {
+        position: relative;
+        font-family: "amatic sc";
+        font-size: ${theme.fonts.P5};
+        font-weight: ${theme.weights.bold};
+        text-align: center;
+        /* border: 1px solid lightgreen; */
+
+        &::after {
+            content: "";
+            position: absolute;
+            height: 3px;
+            width: 100%;
+            bottom: -32px;
+            left: 0%;
+            background-color: ${theme.colors.orange};
+        }
+    }
+
+    .cta-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 5px;
+        /* border: 3px solid yellow; */
+        h3 {
+            font-family: "amatic sc";
+            font-size: ${theme.fonts.P4};
+            /* border: 1px solid yellow; */
+            text-align: center;
+        }
+        .cta-input-button {
+            display: flex;
+            flex-direction: column;
+            /* border: 1px solid lightblue; */
+            width: 100%;
+            gap: ${theme.spacing.md};
+            border-radius: ${theme.spacing.xxs};
+
+            .input-wrapper {
+                background: ${theme.colors.white};
+                display: flex;
+                align-items: center;
+                gap: 5px;
+                padding: 18px 24px;
+                border-radius: ${theme.spacing.xxs};
+
+                .icon-person-circle {
+                    color: ${theme.colors.greySemiDark};
+                }
+            }
+
+            input {
+                border-radius: ${theme.spacing.br};
+            }
+
+            button {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 9px;
+                font-family: Arial;
+                font-size: 15px;
+                font-weight: 700;
+                border: none;
+                border-radius: ${theme.spacing.xxs};
+                padding-block: 18px;
+                color: ${theme.colors.white};
+                background-color: ${theme.colors.primary_burger};
+            }
+        }
+    }
+`;
+
+export default LoginForm;
--- src/components/pages/loginPage/LoginPage.jsx
+++ src/components/pages/loginPage/LoginPage.jsx
@@ -1,11 +1,54 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import LoginForm from "./LoginForm";
 
 function LoginPage() {
-  
-  return (
-       <LoginForm/>
-  )
+    return (
+        <LoginPageStyled>
+            <h1>
+                Crazee
+                <span>
+                    <img src={logo} alt="logo" />
+                </span>
+                Burger
+            </h1>
+            <LoginForm />
+        </LoginPageStyled>
+    );
 }
 
-export default LoginPage
+const LoginPageStyled = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 40px;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+
+    &::before {
+        content: "";
+        background: url(${backgroundImage}) rgba(1, 1, 1, 0.7);
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: darken;
+        position: absolute;
+        inset: 0;
+        z-index: -1;
+    }
+
+    h1 {
+        font-family: "amatic sc";
+        font-size: ${theme.fonts.P7};
+        /* border: 1px solid green; */
+        text-align: center;
+        color: ${theme.colors.primary};
+
+        img {
+            padding-inline: ${theme.spacing.sm};
+            width: 200px;
+            vertical-align: middle;
+        }
+    }
+`;
+export default LoginPage;
