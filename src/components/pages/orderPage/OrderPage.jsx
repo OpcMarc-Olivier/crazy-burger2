482bd1bc10314b3b4dc805c1e6441c565aa753a8
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-07-18 10:32:32 +0200

    🚧  F04: ORDER > layout > Add container

--- src/components/pages/orderPage/OrderPage.jsx
+++ src/components/pages/orderPage/OrderPage.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { NavLink, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function OrderPage() {
     const { userName } = useParams();
     return (
-        <div>
-            <h1>Bonjour {userName}</h1>
-            <NavLink to="/">
-                <button>Déconnexion</button>
-            </NavLink>
-        </div>
+        <OrderPageStyled>
+            <div className="container">
+                <h1>Bonjour {userName}</h1>
+                <NavLink to="/">
+                    <button>Déconnexion</button>
+                </NavLink>
+            </div>
+        </OrderPageStyled>
     );
 }
+
+const OrderPageStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: orange;
+    height: 100vh;
+
+    .container {
+        background: red;
+        height: 95vh;
+        width: 1400px;
+    }
+`;
 
 export default OrderPage;
