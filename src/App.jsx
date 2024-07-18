a400e6ae78366f9cc3f13155eb47d688af583150
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-07-18 10:07:53 +0200

    🔥 F04: delete OrderPage2 and update OrderPage in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-
-import {Route, Routes } from 'react-router-dom'
-import LoginPage from './components/pages/loginPage/LoginPage'
-import OrderPage2 from './components/pages/orderPage/OrderPage2'
-import ErrorPage from './components/pages/errorPage/ErrorPage'
-
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./components/pages/loginPage/LoginPage";
+import OrderPage from "./components/pages/orderPage/OrderPage";
+import ErrorPage from "./components/pages/errorPage/ErrorPage";
 
 function App() {
-  return (
-      <Routes>
-        <Route path='/' element={<LoginPage />}></Route>
-        <Route path='/order/:userName' element={<OrderPage2 />}></Route>
-        <Route path='*' element={<ErrorPage />}></Route>
-      </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+            <Route path="/order/:userName" element={<OrderPage />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
+    );
 }
 
-export default App
+export default App;
--- src/components/pages/orderPage/OrderPage.jsx
+++ src/components/pages/orderPage/OrderPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { NavLink, useParams } from "react-router-dom";
 
 function OrderPage() {
-  return (
-    <div>OrderPage</div>
-  )
+    const { userName } = useParams();
+    return (
+        <div>
+            <h1>Bonjour {userName}</h1>
+            <NavLink to="/">
+                <button>Déconnexion</button>
+            </NavLink>
+        </div>
+    );
 }
 
-export default OrderPage
+export default OrderPage;
