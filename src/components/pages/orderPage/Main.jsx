884621e5ccb9f99f2dc81cc1967df96e29ba8fb2
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-08-02 14:40:10 +0200

    🚧 F05: ORDER > Main > Add grid layout

--- src/components/pages/orderPage/Main.jsx
+++ src/components/pages/orderPage/Main.jsx
@@ -3,15 +3,29 @@ import styled from "styled-components";
 import { theme } from "../../../assets/theme";
 
 function Main() {
-    return <MainStyled></MainStyled>;
+    return (
+        <MainStyled className="main">
+            <div className="basket">Basket</div>
+            <div className="menu">Menu</div>
+        </MainStyled>
+    );
 }
 
 const MainStyled = styled.div`
+    display: grid;
+    grid-template-columns: 25% 1fr;
     flex: 1;
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     background: ${theme.colors.background_white};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+    .basket {
+        background: pink;
+    }
+    .menu {
+        background: purple;
+    }
 `;
 
 export default Main;
