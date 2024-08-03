07f787c80f3897d5f18ccbb71e6738dcb555cb31
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-08-03 10:50:23 +0200

    💄 F05: ORDER > Product > add description layout

--- src/components/pages/orderPage/Main/Product.jsx
+++ src/components/pages/orderPage/Main/Product.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../../assets/theme/index";
 
 function Product({ imageSource, title, price }) {
     return (
@@ -7,11 +8,11 @@ function Product({ imageSource, title, price }) {
             <div className="image">
                 <img src={imageSource} alt={title} />
             </div>
-            <div className="info-text">
+            <div className="text-info">
                 <div className="title"> {title}</div>
                 <div className="description">
-                    <div className="price">{price}</div>
-                    <button className="add-button">Ajouter</button>
+                    <div className="left-description">{price}</div>
+                    <button className="right-description">Ajouter</button>
                 </div>
             </div>
         </ProductStyled>
@@ -19,9 +20,11 @@ function Product({ imageSource, title, price }) {
 }
 const ProductStyled = styled.div`
     background: red;
-    width: 200px;
+    box-sizing: border-box;
+    width: 240px;
     height: 330px;
-    padding: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
     padding-bottom: 10px;
     display: grid;
     grid-template-rows: 65% 1fr;
@@ -37,8 +40,50 @@ const ProductStyled = styled.div`
             object-fit: contain;
         }
     }
-    .info-text {
-        border: 1px solid purple;
+    .text-info {
+        border: 3px solid fuchsia;
+        display: grid;
+        grid-template-rows: 30% 70%;
+        .title {
+            margin: auto 0;
+            font-size: ${theme.fonts.size.P4};
+            position: relative;
+            bottom: 10px;
+            font-weight: ${theme.fonts.weights.bold};
+            color: ${theme.colors.dark};
+            text-align: left;
+            white-space: nowrap;
+            overflow: hidden;
+            width: 100%;
+            text-overflow: ellipsis;
+            font-family: "Amatic SC", cursive;
+        }
+        .description {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            .left-description {
+                border: 1px solid blue;
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+                font-weight: ${theme.fonts.weights.medium};
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                font-weight: ${theme.fonts.weights.medium};
+                color: ${theme.colors.primary};
+            }
+            .right-description {
+                border: 1px solid green;
+                cursor: pointer;
+                border: 1px solid green;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+                font-size: ${theme.fonts.size.P1};
+                cursor: pointer;
+            }
+        }
     }
 `;
 
