f6257d6db0d0697be057543ab78b224237b2e1ee
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-08-03 15:10:00 +0200

    💄 F06: NAVBAR > Right side > Add spacing

--- src/components/pages/orderPage/NavBar/NavBarRight.jsx
+++ src/components/pages/orderPage/NavBar/NavBarRight.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 
 function NavBarRight({ userName }) {
     return (
         <NavBarRightStyled>
-            {/* <div className="admin-button">Admin Button</div> */}
+            <ToggleButton
+                labelIfUnchecked="ACTIVER LE MODE ADMIN"
+                labelIfChecked="DESACTIVER LE MODE ADMIN"
+            />
             <Profile userName={userName} />
         </NavBarRightStyled>
     );
@@ -15,9 +19,8 @@ const NavBarRightStyled = styled.div`
     display: flex;
     align-items: center;
     padding-right: 50px;
-    /* .admin-button {
-        background: red;
-    } */
+    .admin-button {
+    }
 `;
 
 export default NavBarRight;
