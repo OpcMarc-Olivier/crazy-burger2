665d75a5756cefe4f91463b2f7bb4e3f6f296314
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-08-22 14:42:33 +0200

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

---
+++ src/components/pages/orderPage/Main/Admin/AdminTabs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function AdminTabs() {
+    return <AdminTabsStyled>AdminTabs</AdminTabsStyled>;
+}
+
+const AdminTabsStyled = styled.div`
+    border: 1px solid red;
+`;
+
+export default AdminTabs;
