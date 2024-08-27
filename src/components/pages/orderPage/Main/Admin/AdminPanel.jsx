d37df3f96d8b9b3f77571976f7f4f21845153990
OpcMarc-Olivier <mo.perrois@gmail.com>
2024-08-27 13:39:11 +0200

    ♻️ F07: ADMIN > PanelAdmin > Display content based on tabSelected

--- src/components/pages/orderPage/Main/Admin/AdminPanel.jsx
+++ src/components/pages/orderPage/Main/Admin/AdminPanel.jsx
@@ -2,13 +2,18 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { theme } from "../../../../../assets/theme";
 import OrderContext from "../../../../../context/OrderContext";
+import { tabsConfig, getTabSelected } from "./tabsConfig.jsx";
 
 function AdminPanel() {
     const { currentTabSelected } = useContext(OrderContext);
+
+    const tabs = tabsConfig;
+
+    const tabSelected = getTabSelected(tabs, currentTabSelected);
+
     return (
         <AdminPanelStyled>
-            {currentTabSelected === "add" && <p>Ajouter un produit</p>}
-            {currentTabSelected === "edit" && <p>Modifier un produit</p>}
+            <p>{tabSelected.label}</p>
         </AdminPanelStyled>
     );
 }
